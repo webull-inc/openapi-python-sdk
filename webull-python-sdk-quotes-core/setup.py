f94b7e664eb256fa61692f625565d805331227e3
WaOrz <zhigesayhi@outlook.com>
2025-06-10 14:18:13 +0800

    fix: Query Orders History V2

--- webull-python-sdk-quotes-core/setup.py
+++ webull-python-sdk-quotes-core/setup.py
@@ -24,7 +24,7 @@ requires = [
     "grpcio==1.51.1",
     "grpcio-tools==1.51.1",
     "protobuf==4.21.12",
-    "webull-python-sdk-core==0.1.12"
+    "webull-python-sdk-core==0.1.13"
 ]
 
 setup_args = {
